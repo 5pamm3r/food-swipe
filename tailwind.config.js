6a1db8bb4f68d008d1bc5fa9af7bf6463e1fd964
5pamm3r <cmassarino.dev@gmail.com>
2023-07-06 23:42:59 -0300

    add height

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      height: {
+        '400px': '400px',
+        '80vh': '80vh',
+      },
     },
   },
   plugins: [],
