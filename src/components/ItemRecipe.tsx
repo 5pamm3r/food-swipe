367c2cd928a368dfc828100fd3d3ea2ff923681c
5pamm3r <cmassarino.dev@gmail.com>
2023-07-07 23:18:18 -0300

    add drag className

--- src/components/ItemRecipe.tsx
+++ src/components/ItemRecipe.tsx
@@ -1,27 +1,37 @@
-import { url } from 'inspector';
-import React from 'react'
+import React, { useState } from "react";
+import { DraggableProps } from "react-draggable";
 
 interface Props {
   title: string;
   description: string;
   imageUrl: string;
-  link: string
+  link: string;
+  drag: string;
 }
-const ItemRecipe = ({ title, description, imageUrl, link }: Props): JSX.Element => {
+const ItemRecipe: React.FC<Props> = ({
+  title,
+  description,
+  imageUrl,
+  link,
+  drag,
+}) => {
   return (
-    <>
-      <li className='border-2 border-black p-2 absolute h-full w-full max-w-lg flex items-center justify-center'>
-        <a href={link} target='_blank'>
-          <div className='h-400px'>
-            <div className='bg-center bg-cover h-full' style={{ backgroundImage: `url(${imageUrl})` }}>
-            </div>
-          </div>
-          <p className='font-bold mb-2'>{title}</p>
-          <p className='line-clamp-3 text-gray-500'>{description}</p>
-        </a>
-      </li>
-    </>
-  )
-}
+    <li
+      className={`h-full w-full flex items-center justify-center select-none touch-none ${drag} border border-black p-2`}
+    >
+      <div className="h-full">
+        <div className="h-60vh">
+          <div
+            className="bg-center bg-cover h-full"
+            style={{ backgroundImage: `url(${imageUrl})` }}
+          ></div>
+        </div>
+        <p className="font-bold mb-2">{title}</p>
+        <p className="line-clamp-3 text-gray-500">{description}</p>
+        <a href={link} className="underline" rel="noopener noreferrer">Go recipe</a>
+      </div>
+    </li>
+  );
+};
 
-export default ItemRecipe
+export default ItemRecipe;
