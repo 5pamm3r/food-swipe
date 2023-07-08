2dc8b4243c8ffef314d641307cdee1c107321e1c
5pamm3r <cmassarino.dev@gmail.com>
2023-07-07 23:16:38 -0300

    add drag effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 "use client";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { getRecipes } from "@/Api/recipes";
-import { Post } from "@/Api/type";
+import { Post } from "@/types/Post";
 import ItemRecipe from "@/components/ItemRecipe";
 import Buttons from "@/components/Buttons";
 import Index from "@/types/Index";
+import Draggable, { DraggableData, DraggableEvent, DraggableEventHandler, DraggableProps } from "react-draggable";
+import { mockData } from '@/Api/mock';
 
+interface DragState {
+  activeDrags: number;
+  deltaPosition: { x: number; y: number; };
+  controlledPosition: { x: number; y: number; };
+}
 export default function Home() {
   const [recipes, setRecipes] = useState<Post[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const recipesData = await getRecipes();
-        setRecipes(recipesData);
+        // const recipes = await getRecipes();
+        const recipes = await mockData();
+        setRecipes(recipes)
       } catch (error) {
         console.error(error);
       }
@@ -22,22 +30,76 @@ export default function Home() {
     fetchData();
   }, []);
 
-  const [index, setIndex] = useState<Index>({ startIndex: 0, endIndex: 4 })
-
+  const [index, setIndex] = useState<Index>({ startIndex: 0, endIndex: 1 })
+  const [dragState, setDragState] = useState<DragState>({
+    activeDrags: 0,
+    deltaPosition: {
+      x: 0, y: 0
+    },
+    controlledPosition: {
+      x: 0, y: 0
+    }
+  });
+  const onStart = () => {
+    setDragState((prevState) => ({
+      ...prevState,
+      activeDrags: ++dragState.activeDrags
+    }));
+  };
+  const onStop = () => {
+    setDragState((prevState) => ({
+      ...prevState,
+      activeDrags: --dragState.activeDrags
+    }));
+  }
+  const handleDrag = (e: DraggableEvent, ui: DraggableData) => {
+    const { x, y } = dragState.deltaPosition;
+    setDragState((prevState) => ({
+      ...prevState,
+      deltaPosition: {
+        x: x + ui.deltaX,
+        y: y + ui.deltaY,
+      }
+    }));
+    console.log(ui.deltaX, ui.deltaY)
+  }
+  // const onControlledDrag = (e: DraggableEvent, position: DraggableData) => {
+  //   const { x, y } = position;
+  //   setDragState((prevState) => ({
+  //     ...prevState,
+  //     controlledPosition: { x, y }
+  //   }));
+  // };
+  // const onControlledDragStop = (e: DraggableEvent, position: DraggableData) => {
+  //   onControlledDrag(e, position);
+  //   onStop();
+  // };
+  const dragHandlers = { onStart: onStart, onStop: onStop };
   return (
-    <main className='grid content-center h-screen'>
-      <ul className="m-auto relative w-full relative h-80vh flex items-center justify-center">
+    <main className='flex flex-col justify-between h-full'>
+      <ul className="p-2">
         {recipes.slice(index.startIndex, index.endIndex).map((recipe: Post, index: number) => (
-          <ItemRecipe
+          <Draggable
             key={index}
-            title={recipe.yoast_head_json.title}
-            description={recipe.yoast_head_json.description}
-            imageUrl={recipe.yoast_head_json.og_image?.[0]?.url}
-            link={recipe.yoast_head_json.og_url}
-          />
+            {...dragHandlers}
+            onDrag={handleDrag}
+            position={dragState.controlledPosition}
+          // onStop={onControlledDragStop}
+          >
+            <div className='w-full h-full'>
+              <ItemRecipe
+                drag='draggable-handle'
+                title={recipe.yoast_head_json.title}
+                description={recipe.yoast_head_json.description}
+                imageUrl={recipe.yoast_head_json.og_image?.[0]?.url}
+                link={recipe.yoast_head_json.og_url}
+              />
+              <p>x: ${dragState.deltaPosition.x.toFixed()} y: ${dragState.deltaPosition.y.toFixed()}</p>
+            </div>
+          </Draggable>
         ))}
       </ul>
       <Buttons setIndex={setIndex} />
-    </main>
+    </main >
   );
 }
