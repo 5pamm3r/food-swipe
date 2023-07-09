dd678de065f51b46c129f918e5514a698210c445
5pamm3r <cmassarino.dev@gmail.com>
2023-07-08 23:03:56 -0300

    add provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import RecipeProvider from '@/context/recipeContext'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <RecipeProvider>
+          {children}
+        </RecipeProvider>
+      </body>
     </html>
   )
 }
