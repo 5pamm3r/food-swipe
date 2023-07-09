e213da138271deb601c5ee8a417ada90f9c5c395
5pamm3r <cmassarino.dev@gmail.com>
2023-07-08 23:04:59 -0300

    fix style

--- src/components/ItemRecipe.tsx
+++ src/components/ItemRecipe.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import { DraggableProps } from "react-draggable";
+import React from "react";
 
 interface Props {
   title: string;
@@ -17,18 +16,20 @@ const ItemRecipe: React.FC<Props> = ({
 }) => {
   return (
     <li
-      className={`h-full w-full flex items-center justify-center select-none touch-none ${drag} border border-black p-2`}
+      className={`h-full w-full flex items-center justify-center select-none touch-none ${drag} p-px bg-white rounded-2xl`}
     >
       <div className="h-full">
         <div className="h-60vh">
           <div
-            className="bg-center bg-cover h-full"
+            className="bg-center bg-cover h-full rounded-t-2xl"
             style={{ backgroundImage: `url(${imageUrl})` }}
           ></div>
         </div>
-        <p className="font-bold mb-2">{title}</p>
-        <p className="line-clamp-3 text-gray-500">{description}</p>
-        <a href={link} className="underline" rel="noopener noreferrer">Go recipe</a>
+        <div className='p-4'>
+          <p className="font-bold mb-2">{title}</p>
+          <p className="line-clamp-3 text-gray-500">{description}</p>
+          <a href={link} className="underline mt-4" rel="noopener noreferrer" target='_blank'>Visit</a>
+        </div>
       </div>
     </li>
   );
