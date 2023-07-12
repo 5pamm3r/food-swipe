8dbc3430470d49ff9ccd4fa89a9c73fec50ee2dd
5pamm3r <cmassarino.dev@gmail.com>
2023-07-11 23:55:11 -0300

    fix style

--- src/components/ItemRecipe.tsx
+++ src/components/ItemRecipe.tsx
@@ -15,8 +15,8 @@ const ItemRecipe: React.FC<Props> = ({
   drag,
 }) => {
   return (
-    <li
-      className={`h-full w-full flex items-center justify-center select-none touch-none ${drag} p-px bg-white rounded-2xl`}
+    <div
+      className={`tall:h-750 w-full flex items-center justify-center select-none touch-none ${drag} p-px bg-white rounded-2xl`}
     >
       <div className="h-full">
         <div className="h-60vh">
@@ -25,13 +25,23 @@ const ItemRecipe: React.FC<Props> = ({
             style={{ backgroundImage: `url(${imageUrl})` }}
           ></div>
         </div>
-        <div className='p-4'>
-          <p className="font-bold mb-2">{title}</p>
+        <div className="p-4">
+          <p className="font-bold mb-2 line-clamp-1 tall:line-clamp-none">{title}</p>
           <p className="line-clamp-3 text-gray-500">{description}</p>
-          <a href={link} className="underline mt-4" rel="noopener noreferrer" target='_blank'>Visit</a>
+          <a
+            href={link}
+            className="underline mt-2 block w-fit"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <i>
+
+              Ir a la receta
+            </i>
+          </a>
         </div>
       </div>
-    </li>
+    </div>
   );
 };
 
