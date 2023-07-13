25e5b8d2cf98dffc42a19c5dc90641329199dae3
5pamm3r <cmassarino.dev@gmail.com>
2023-07-12 23:26:03 -0300

    parse recipes

--- src/Api/recipes.ts
+++ src/Api/recipes.ts
@@ -1,30 +1,11 @@
 import axios from "axios";
-import { Recipe } from "../types/Recipe";
+import { RawRecipe } from "../types/RawRecipe";
+import { parseRecipe } from "./parseRecipes";
 
 export async function getRecipes() {
   try {
-    const response = await axios.get<Recipe[]>('https://www.cocinacaserayfacil.net/wp-json/wp/v2/posts?&per_page=10');
-    const posts: Recipe[] = response.data;
-
-    const extractedData = posts.map(post => {
-      const { yoast_head_json } = post;
-      const id = post.id;
-      const postTitle = yoast_head_json.og_title;
-      const postDesc = yoast_head_json.og_description;
-      const og_url = yoast_head_json.og_url;
-      const og_image = yoast_head_json.og_image;
-      const imageUrl = og_image && og_image.length > 0 ? og_image[0].url : null;
-
-      return {
-        id: id,
-        title: postTitle,
-        description: postDesc,
-        og_url: og_url,
-        imageUrl: imageUrl,
-        yoast_head_json: yoast_head_json
-      };
-    });
-    return extractedData;
+    const recipes = await axios.get<RawRecipe[]>('https://www.cocinacaserayfacil.net/wp-json/wp/v2/posts?&per_page=10').then((res) => parseRecipe(res.data));
+    return recipes;
   } catch (error) {
     console.error(error);
     throw error;
