7b166ec1267c1ee0cdaf0fcd658ce178aa1ae931
5pamm3r <cmassarino.dev@gmail.com>
2023-07-12 23:30:34 -0300

    fix types

--- src/types/Recipe.ts
+++ src/types/Recipe.ts
@@ -1,14 +1,7 @@
 export interface Recipe {
   id: string;
-  yoast_head_json: {
-    og_title: string;
-    og_description: string;
-    og_url: string;
-    og_image: {
-      width: number;
-      height: number;
-      url: string;
-      type: string;
-    }[];
-  };
+  title: string;
+  description: string;
+  url: string;
+  imageUrl: string;
 }
