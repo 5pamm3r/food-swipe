8b955ac24971c2b61636c8d0668f9951c8bb5477
5pamm3r <cmassarino.dev@gmail.com>
2023-07-13 22:06:49 -0300

    add random recipies

--- src/Api/recipes.ts
+++ src/Api/recipes.ts
@@ -4,8 +4,8 @@ import { parseRecipe } from "./parseRecipes";
 
 export async function getRecipes() {
   try {
-    const recipes = await axios.get<RawRecipe[]>('https://www.cocinacaserayfacil.net/wp-json/wp/v2/posts?&per_page=10').then((res) => parseRecipe(res.data));
-    return recipes;
+    const recipes = await axios.get<RawRecipe[]>('https://www.cocinacaserayfacil.net/wp-json/wp/v2/posts?&per_page=100').then((res) => parseRecipe(res.data));
+    return recipes.sort(() => 0.5 - Math.random())
   } catch (error) {
     console.error(error);
     throw error;
