43bf1920c328c8291db681895a5e4883329463be
5pamm3r <cmassarino.dev@gmail.com>
2023-07-13 22:10:20 -0300

    add reset button

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import { RecipeContext } from '@/context/recipeContext';
+import { Recipe } from '@/types/Recipe';
+import React, { useContext } from 'react';
 
 interface Props {
   modalActive: boolean;
   setModalActive: React.Dispatch<React.SetStateAction<boolean>>;
+  setRecipes: React.Dispatch<React.SetStateAction<Recipe[]>>;
 }
-const Buttons = ({ modalActive, setModalActive }: Props): JSX.Element => {
+const Buttons = ({ modalActive, setModalActive, setRecipes }: Props): JSX.Element => {
+  const {
+    state: { originalRecipes }
+  } = useContext(RecipeContext);
 
   const onOpenModal = () => {
     setModalActive(!modalActive)
   }
+  const onReset = () => {
+    setRecipes(originalRecipes)
+  }
   return (
-    <button className='p-2 mx-auto absolute bottom-2 left-0 right-0 w-2/4  bg-red-500 rounded-lg' type='button' onClick={onOpenModal}>Favoritos</button>
+    <div className='mx-auto absolute bottom-2 left-0 right-0 px-2 flex justify-evenly'>
+      <button className='w-1/4 p-2 bg-slate-500 rounded-lg' onClick={onReset}>Reiniciar</button>
+      <button className='w-1/4 p-2  bg-red-500 rounded-lg' type='button' onClick={onOpenModal}>Favoritos</button>
+    </div>
   )
 }
 
