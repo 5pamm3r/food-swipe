aff32b4d32353c1366efe0089e6060886dd07cf0
5pamm3r <cmassarino.dev@gmail.com>
2023-07-15 20:15:24 -0300

    fix path

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@ import { RecipeContext } from '@/context/recipeContext';
 import Link from 'next/link'
 import React, { useContext, useEffect } from 'react'
 
-const Login = (): JSX.Element => {
+export function Login(): JSX.Element {
   const {
     actions: { setLogin, saveChef }
   } = useContext(RecipeContext);
@@ -20,9 +20,9 @@ const Login = (): JSX.Element => {
   }, [])
   return (
     <div className='flex flex-col justify-center h-full w-full items-center bg-white rounded-lg gap-4'>
-      <h1>Elige tu cocinero o cocinera favorito</h1>
-      <Link href='/home' className='text-center w-96 bg-red-400 rounded-lg py-3' onClick={onClickPaulina}>Paulina Cocina</Link>
-      <Link href='/home' className='text-center w-96 bg-blue-400 rounded-lg py-3' onClick={onClickGorka}>Gorka Barredo</Link>
+      <h1 className='font-bold text-lg mb-4'>Elige tu cocinero o cocinera favorito</h1>
+      <Link href='/main' className='text-center w-96 bg-red-400 rounded-lg py-3' onClick={onClickPaulina}>Paulina Cocina</Link>
+      <Link href='/main' className='text-center w-96 bg-blue-400 rounded-lg py-3' onClick={onClickGorka}>Gorka Barredo</Link>
     </div>
   )
 }
