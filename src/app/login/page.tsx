889ec52a0ddcded9638066a9fe7b488e05cb91a4
5pamm3r <cmassarino.dev@gmail.com>
2023-07-14 23:48:37 -0300

    add links

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+'use client'
+import { RecipeContext } from '@/context/recipeContext';
+import Link from 'next/link'
+import React, { useContext, useEffect } from 'react'
 
 const Login = (): JSX.Element => {
+  const {
+    actions: { setLogin, saveChef }
+  } = useContext(RecipeContext);
+  const onClickGorka = () => {
+    setLogin((prevState) => !prevState);
+    saveChef('Gorka')
+  }
+  const onClickPaulina = () => {
+    setLogin((prevState) => !prevState);
+    saveChef('Paulina')
+  }
+  useEffect(() => {
+    setLogin(false)
+  }, [])
   return (
     <div className='flex flex-col justify-center h-full w-full items-center bg-white rounded-lg gap-4'>
       <h1>Elige tu cocinero o cocinera favorito</h1>
-      <button className='w-96 bg-red-400 rounded-lg py-3'>Paulina Cocina</button>
-      <button className='w-96 bg-blue-400 rounded-lg py-3 '>Gorka Barredo</button>
+      <Link href='/home' className='text-center w-96 bg-red-400 rounded-lg py-3' onClick={onClickPaulina}>Paulina Cocina</Link>
+      <Link href='/home' className='text-center w-96 bg-blue-400 rounded-lg py-3' onClick={onClickGorka}>Gorka Barredo</Link>
     </div>
   )
 }
