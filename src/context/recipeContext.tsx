ca54e7211e4b35a608f9eaaef17552216592c149
5pamm3r <cmassarino.dev@gmail.com>
2023-07-14 23:49:24 -0300

    add login and chefs state

--- src/context/recipeContext.tsx
+++ src/context/recipeContext.tsx
@@ -1,7 +1,9 @@
 'use client'
 import { getRecipes } from "@/Api/recipes";
 import { useLocalStorage } from "@/localStorage/useLocalStorage";
+import { RawRecipe } from "@/types/RawRecipe";
 import { Recipe } from "@/types/Recipe";
+import { get } from "http";
 import React, { createContext, useEffect, useState } from "react";
 
 interface Context {
@@ -11,6 +13,8 @@ interface Context {
   };
   actions: {
     saveFavorites: (recipes: Recipe[]) => void;
+    setLogin: React.Dispatch<React.SetStateAction<boolean>>;
+    saveChef: (chef: string) => void;
   };
 }
 export const RecipeContext = createContext({} as Context);
@@ -21,24 +25,38 @@ export const RecipeProvider = ({ children }: { children: React.ReactNode }) => {
     setFavorites(recipes);
   };
   const [originalRecipes, setOriginalRecipes] = useState<Recipe[]>([]);
-
+  const [login, setLogin] = useState<boolean>(false);
+  const [chef, setChef] = useState<string>('')
+  const saveChef = (chef: string) => {
+    setChef(chef)
+  }
   useEffect(() => {
     (async function () {
       try {
-        const recipes = await getRecipes();
+        if (!login) {
+          return
+        }
+        let recipes: Recipe[] = []
+        if (chef === 'Gorka') {
+          recipes = await getRecipes({ chef });
+        } else if (chef === 'Paulina') {
+          recipes = await getRecipes({ chef });
+        }
         // const recipes = await mockData();
         setOriginalRecipes(recipes);
       } catch (error) {
         console.error(error);
       }
     })()
-  }, []);
+  }, [login, chef]);
   const state: Context["state"] = {
     favorites,
     originalRecipes,
   };
   const actions: Context["actions"] = {
     saveFavorites,
+    setLogin,
+    saveChef,
   };
 
   return (
