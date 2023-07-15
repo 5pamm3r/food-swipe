cdb80a5c147dce82c36797a1c5151350ec21fb93
5pamm3r <cmassarino.dev@gmail.com>
2023-07-15 20:19:09 -0300

    add skeleton

---
+++ src/components/recipes/skeleton.tsx
@@ -0,0 +1,7 @@
+export function SkeletonItemRecipe(): JSX.Element {
+  return (
+    <div className="animate-pulse">
+      <div className="h-80vh bg-gray-300 rounded-2xl tall:h-750 mt-4"></div>
+    </div>
+  )
+}
