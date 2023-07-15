6838663c61f8931af634af5b4e41986dff19d0b0
5pamm3r <cmassarino.dev@gmail.com>
2023-07-14 23:50:14 -0300

    initial commit

---
+++ src/types/RawPaulina.ts
@@ -0,0 +1,13 @@
+export interface RawPaulina {
+  id: string;
+  title: {
+    rendered: string;
+  }
+  excerpt: {
+    rendered: string;
+  }
+  link: string;
+  content: {
+    rendered: string;
+  }
+}
