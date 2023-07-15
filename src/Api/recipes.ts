be0c44ec9a2a417bbdb23a3a8966286e129ee883
5pamm3r <cmassarino.dev@gmail.com>
2023-07-14 23:46:48 -0300

    add get to paulina cocina

--- src/Api/recipes.ts
+++ src/Api/recipes.ts
@@ -1,11 +1,19 @@
 import axios from "axios";
 import { RawRecipe } from "../types/RawRecipe";
 import { parseRecipe } from "./parseRecipes";
+import { RawPaulina } from "@/types/RawPaulina";
+import { parsePaulina } from "./parsePaulina";
 
-export async function getRecipes() {
+export async function getRecipes({ chef }: { chef?: string }) {
   try {
-    const recipes = await axios.get<RawRecipe[]>('https://www.cocinacaserayfacil.net/wp-json/wp/v2/posts?&per_page=100').then((res) => parseRecipe(res.data));
-    return recipes.sort(() => 0.5 - Math.random())
+    if (chef === 'Gorka') {
+      const recipes = await axios.get<RawRecipe[]>('https://www.cocinacaserayfacil.net/wp-json/wp/v2/posts?&per_page=100').then((res) => parseRecipe(res.data));
+      return recipes.sort(() => 0.5 - Math.random())
+
+    } else {
+      const recipes = await axios.get<RawPaulina[]>('https://www.paulinacocina.net/wp-json/wp/v2/posts?&per_page=100').then((res) => parsePaulina(res.data));
+      return recipes.sort(() => 0.5 - Math.random())
+    }
   } catch (error) {
     console.error(error);
     throw error;
