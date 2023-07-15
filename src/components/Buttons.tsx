271f27ddf803693d780a04c9d9f983d1e7a406a0
5pamm3r <cmassarino.dev@gmail.com>
2023-07-14 23:48:52 -0300

    add next link

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,5 +1,6 @@
 import { RecipeContext } from '@/context/recipeContext';
 import { Recipe } from '@/types/Recipe';
+import Link from 'next/link';
 import React, { useContext } from 'react';
 
 interface Props {
@@ -22,6 +23,7 @@ const Buttons = ({ modalActive, setModalActive, setRecipes }: Props): JSX.Elemen
     <div className='mx-auto absolute bottom-2 left-0 right-0 px-2 flex justify-evenly'>
       <button className='w-1/4 p-2 bg-slate-500 rounded-lg' onClick={onReset}>Reiniciar</button>
       <button className='w-1/4 p-2  bg-red-500 rounded-lg' type='button' onClick={onOpenModal}>Favoritos</button>
+      <Link href='/' className='w-1/4 p-2 bg-slate-500 rounded-lg text-center'>Chefs</Link>
     </div>
   )
 }
