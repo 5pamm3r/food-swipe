160fd6bbd55216d85cfe3c195c6fb06a4b946b44
5pamm3r <cmassarino.dev@gmail.com>
2023-07-18 22:53:40 -0300

    reduce post per page

--- src/Api/recipes.ts
+++ src/Api/recipes.ts
@@ -7,11 +7,11 @@ import { parsePaulina } from "./parsePaulina";
 export async function getRecipes({ chef }: { chef?: string }) {
   try {
     if (chef === 'Gorka') {
-      const recipes = await axios.get<RawGorka[]>('https://www.cocinacaserayfacil.net/wp-json/wp/v2/posts?&per_page=100').then((res) => parseGorka(res.data));
+      const recipes = await axios.get<RawGorka[]>('https://www.cocinacaserayfacil.net/wp-json/wp/v2/posts?&per_page=50').then((res) => parseGorka(res.data));
       return recipes.sort(() => 0.5 - Math.random())
 
     } else {
-      const recipes = await axios.get<RawPaulina[]>('https://www.paulinacocina.net/wp-json/wp/v2/posts?&per_page=100').then((res) => parsePaulina(res.data));
+      const recipes = await axios.get<RawPaulina[]>('https://www.paulinacocina.net/wp-json/wp/v2/posts?&per_page=50').then((res) => parsePaulina(res.data));
       return recipes.sort(() => 0.5 - Math.random())
     }
   } catch (error) {
